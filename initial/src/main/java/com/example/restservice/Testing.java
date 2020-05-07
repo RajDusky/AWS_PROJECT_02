4eaeeb1811feba439f4bfa4f368cc61bfc02a319
debraj1976 <De365600@@>
2020-05-06 20:40:18 -0500

    first push

---
+++ initial/src/main/java/com/example/restservice/Testing.java
@@ -0,0 +1,10 @@
+package com.example.restservice;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		System.out.println("Hello From Tamil!!!");
+
+	}
+
+}
---
+++ initial/src/test/java/com/example/restservice/RestServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.restservice;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RestServiceApplicationTests {
+
+	public void contextLoads() {
+	}
+
+}
